37becc5cca2249ecdd70120a550aa694c3eddccd
fiozelaya <fiorella.zelaya1024@gmail.com>
2025-06-22 23:25:08 -0600

    Eliminando archivos innecesarios

--- install.cmd
+++
@@ -1,3 +0,0 @@
-docker build -t multiagent-rag .
-docker run --rm -p 8000:8000 -p 5000:5000 multiagent-rag
-
--- install.sh
+++
@@ -1,3 +0,0 @@
-docker login
-docker build -t multiagent-rag .
-docker run --rm -p 8000:8000 -p 5000:5000 multiagent-rag
